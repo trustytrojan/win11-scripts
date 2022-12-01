c0807ad80c5525f25d154f0929ebd2d0037c7d90
trustytrojan <87675609+trustytrojan@users.noreply.github.com>
2022-11-30 21:09:11 -0800

    Update initial-script.ps1

--- initial-script.ps1
+++ initial-script.ps1
@@ -15,3 +15,6 @@ New-Item -Path "HKCU:\SOFTWARE\Classes\CLSID" -Force | Out-Null
 New-Item -Path "HKCU:\SOFTWARE\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -Force | Out-Null
 New-Item -Path "HKCU:\SOFTWARE\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Force -Value "" | Out-Null
 Restart-Process -Process explorer
+
+# delete this annoying start menu entry
+rm "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\System Tools\Character Map.lnk"

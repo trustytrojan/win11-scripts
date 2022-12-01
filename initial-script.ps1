5b4494f4c3378da54c922ef60d80616619fc4ce6
trustytrojan <87675609+trustytrojan@users.noreply.github.com>
2022-11-30 21:04:00 -0800

    Update and rename uninstall-ms-apps.ps1 to initial-script.ps1

---
+++ initial-script.ps1
@@ -0,0 +1,17 @@
+# uninstalls all apps except the ones i like to have
+foreach($app in (Get-AppxPackage)) {
+  switch($app.name) {
+    "Microsoft.WindowsStore" {}
+    "Microsoft.DesktopAppInstaller" {}
+    "Microsoft.WindowsNotepad" {}
+    "Microsoft.ScreenSketch" {}
+    default { Remove-AppxPackage $app }
+  }
+}
+
+# enables old context menu
+# credit to https://github.com/teeotsa/windows-11-debloat
+New-Item -Path "HKCU:\SOFTWARE\Classes\CLSID" -Force | Out-Null
+New-Item -Path "HKCU:\SOFTWARE\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -Force | Out-Null
+New-Item -Path "HKCU:\SOFTWARE\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Force -Value "" | Out-Null
+Restart-Process -Process explorer
